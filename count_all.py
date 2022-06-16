442c331961c6ad6054e55e4342e673cea02c0af9
Ikrom <ikromyusupov09@gmail.com>
2022-06-16 21:36:08 +0500

    dictionary with letters and digits

--- count_all.py
+++ count_all.py
@@ -1,10 +1,16 @@
 def count_all(txt):
     """
-    Given a function that takes a string and calculates the number of letters and digits within it.
     Return the result in a dictionary.
     Args:
         txt(str): count letters and digits
     Returns:
         dict: dictionary with letters and digits
     """
-    return
+    res = {"LETTERS": 0, "DIGITS": 0}
+    for i in txt:
+        if i.isdigit():
+            res['DIGITS'] += 1
+        elif i.isalpha():
+            res['LETTERS'] += 1
+    return res
+    
